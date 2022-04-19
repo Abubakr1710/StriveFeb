56417a79c0a44df79b2c7772181c59070833572d
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-04-19 18:08:07 +0500

    Update train.py

--- Chapter 02/07. Gradient Boosting and Encoding/train.py
+++ Chapter 02/07. Gradient Boosting and Encoding/train.py
@@ -1,5 +1,5 @@
 import data_handler as dh
 
-x_train, x_test, y_train, y_test = dh.get_data("./insurance.csv")
+x_train, x_test, y_train, y_test = dh.get_data("Chapter 02/07. Gradient Boosting and Encoding/insurance.csv")
 
 print(dh.hello)
