5663137e0cd58119b46d11df28930cc7218c37f7
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-04-19 18:43:56 +0500

    Update train.py

--- Chapter 02/07. Gradient Boosting and Encoding/train.py
+++ Chapter 02/07. Gradient Boosting and Encoding/train.py
@@ -1,4 +1,6 @@
 import data_handler as dh
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor
+from xgboost import XGBRegressor
 
 x_train, x_test, y_train, y_test = dh.get_data("Chapter 02/07. Gradient Boosting and Encoding/insurance.csv")
 
