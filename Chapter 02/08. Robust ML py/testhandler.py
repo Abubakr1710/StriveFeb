b02fb7bf24caf31d5857bbf1202357bc0e8fed25
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-04-22 10:43:14 +0500

    updated
    Added new files

---
+++ Chapter 02/08. Robust ML py/testhandler.py
@@ -0,0 +1,44 @@
+from pyexpat import model
+from sklearn.impute import  SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn import pipeline
+from sklearn import compose
+
+
+
+from sklearn.tree          import DecisionTreeClassifier
+from sklearn.ensemble      import RandomForestClassifier
+from sklearn.ensemble      import ExtraTreesClassifier
+from sklearn.ensemble      import AdaBoostClassifier
+from sklearn.ensemble      import GradientBoostingClassifier
+from sklearn.experimental  import enable_hist_gradient_boosting # Necesary for HistGradientBoostingClassifier
+from sklearn.ensemble      import HistGradientBoostingClassifier
+from xgboost               import XGBClassifier
+from lightgbm              import LGBMClassifier
+from catboost              import CatBoostClassifier
+
+
+
+def tree_classifiers():
+    tree_classifiers = {
+    "Decision Tree": DecisionTreeClassifier(),
+    "Extra Trees": ExtraTreesClassifier(),
+    "Random Forest": RandomForestClassifier(),
+    "AdaBoost": AdaBoostClassifier(),
+    "Skl GBM": GradientBoostingClassifier(),
+    "Skl HistGBM":HistGradientBoostingClassifier(),
+    "XGBoost": XGBClassifier(),
+    "LightGBM": LGBMClassifier(),
+    "CatBoost": CatBoostClassifier()}
+
+    cat_vars  = ['Sex', 'Embarked', 'Title']
+    num_vars  = ['Pclass', 'SibSp', 'Parch', 'Fare', 'Age']
+    num_prepro = pipeline.Pipeline(steps=[('imputer', SimpleImputer(strategy='most_frequent'))])
+    cat = pipeline.Pipeline(steps=[('imputer', SimpleImputer(strategy='most_frequent')),('ohe', OneHotEncoder(handle_unknown='ignore'))])
+    tree_pre  = compose.ColumnTransformer(transformers=[('num', num_prepro, num_vars),('cat', cat, cat_vars)], remainder='drop')
+
+    tree_classifiers = {name: pipeline.make_pipeline(tree_pre, model) for name, model in tree_classifiers.items()}
+    return tree_classifiers
+
+
+
--- Chapter 02/08. Robust ML py/train.py
+++ Chapter 02/08. Robust ML py/train.py
@@ -1,10 +1,12 @@
 from turtle import pd
 import pandas as pd
-
+import testhandler as dh
+import time
+from sklearn import metrics
 
 #Loading data
-df      = pd.read_csv("train.csv", index_col='PassengerId')
-df_test = pd.read_csv("test.csv",  index_col='PassengerId')
+df      = pd.read_csv("Chapter 02/08. Robust ML py/train.csv", index_col='PassengerId')
+df_test = pd.read_csv("Chapter 02/08. Robust ML py/test.csv",  index_col='PassengerId')
 
 #Extract the title
 get_Title_from_Name = lambda x : x.split(',')[1].split('.')[0].strip()
@@ -39,3 +41,30 @@ y = df["Survived"] # 0 = No, 1 = Yes
 
 x_test = df_test.drop(columns=['Name', 'Ticket', 'Cabin']) # # X_TEST DATA (NEW DATA)
 
+from sklearn.model_selection import train_test_split
+X_train, X_val, y_train, y_val = train_test_split(x, y, train_size=0.8, random_state=0, stratify=y)
+
+
+results = pd.DataFrame({'Model': [], 'Accuracy': [], 'Bal Acc.': [], 'Time': []})
+tree_classifier =dh.tree_classifiers()
+
+for model_name, model in tree_classifier.items():
+    start_time = time.time()
+    
+    # FOR EVERY PIPELINE (PREPRO + MODEL) -> TRAIN WITH TRAIN DATA (x_train)
+    
+    model.fit(X_train,y_train)
+    # GET PREDICTIONS USING x_val
+    pred =model.predict(X_val)
+
+    total_time = time.time() - start_time
+
+    results = results.append({"Model":    model_name,
+                              "Accuracy": metrics.accuracy_score(y_val, pred)*100,
+                              "Bal Acc.": metrics.balanced_accuracy_score(y_val, pred)*100,
+                              "Time":     total_time},
+                              ignore_index=True)
+                              
+results_ord = results.sort_values(by=['Accuracy'], ascending=False, ignore_index=True)
+results_ord.index += 1 
+print(results_ord.style.bar(subset=['Accuracy', 'Bal Acc.'], vmin=0, vmax=100, color='#5fba7d'))
