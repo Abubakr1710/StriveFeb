f1d2400a234bebbf2cb0752ab8923d22443378f6
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-04-26 16:14:10 +0500

    Update data_enhancement.py

--- Chapter 02/11. Data Enhancement/data_enhancement.py
+++ Chapter 02/11. Data Enhancement/data_enhancement.py
@@ -23,7 +23,7 @@ from xgboost               import XGBRegressor
 from lightgbm              import LGBMRegressor
 from catboost              import CatBoostRegressor
 
-data = pd.read_csv(r'data\london_merged.csv')
+data = pd.read_csv('Chapter 02/11. Data Enhancement/london_merged.csv')
 
 np.random.seed(0)
 
@@ -95,91 +95,91 @@ print(gen.head(3) )
 
 #print(gen.shape)
 
-#final_data = data
-y = data['cnt']
-x = data.drop(['cnt'], axis=1)
+# #final_data = data
+# y = data['cnt']
+# x = data.drop(['cnt'], axis=1)
 
 
 
-#print(data.shape)
+# #print(data.shape)
 
 
 
 
-cat_vars = ['season','is_weekend','is_holiday','year','month','weather_code']
-num_vars = ['t1','t2','hum','wind_speed']
+# cat_vars = ['season','is_weekend','is_holiday','year','month','weather_code']
+# num_vars = ['t1','t2','hum','wind_speed']
 
 
-x_train, x_val, y_train, y_val = model_selection.train_test_split(x, y,
-                                    test_size=0.2,
-                                    random_state=0  # Recommended for reproducibility
-                                )
-
-
-
-extra_sample = gen.sample(gen.shape[0] // 3)
-x_train = pd.concat([x_train, extra_sample.drop(['cnt'], axis=1 ) ])
-y_train = pd.concat([y_train, extra_sample['cnt'] ])
-
-
-transformer = preprocessing.PowerTransformer()
-y_train = transformer.fit_transform(y_train.values.reshape(-1,1))
-y_val = transformer.transform(y_val.values.reshape(-1,1))
+# x_train, x_val, y_train, y_val = model_selection.train_test_split(x, y,
+#                                     test_size=0.2,
+#                                     random_state=0  # Recommended for reproducibility
+#                                 )
 
 
 
-rang = abs(y_train.max()) + abs(y_train.min())
+# extra_sample = gen.sample(gen.shape[0] // 3)
+# x_train = pd.concat([x_train, extra_sample.drop(['cnt'], axis=1 ) ])
+# y_train = pd.concat([y_train, extra_sample['cnt'] ])
 
-num_4_treeModels = pipeline.Pipeline(steps=[
-    ('imputer', impute.SimpleImputer(strategy='constant', fill_value=-9999)),
-])
 
-cat_4_treeModels = pipeline.Pipeline(steps=[
-    ('imputer', impute.SimpleImputer(strategy='constant', fill_value='missing')),
-    ('ordinal', preprocessing.OrdinalEncoder()) # handle_unknown='ignore' ONLY IN VERSION 0.24
-])
+# transformer = preprocessing.PowerTransformer()
+# y_train = transformer.fit_transform(y_train.values.reshape(-1,1))
+# y_val = transformer.transform(y_val.values.reshape(-1,1))
 
-tree_prepro = compose.ColumnTransformer(transformers=[
-    ('num', num_4_treeModels, num_vars),
-    ('cat', cat_4_treeModels, cat_vars),
-], remainder='drop') # Drop other vars not specified in num_vars or cat_vars
 
-tree_classifiers = {
-  "Decision Tree": DecisionTreeRegressor(),
-  "Extra Trees":   ExtraTreesRegressor(n_estimators=100),
-  "Random Forest": RandomForestRegressor(n_estimators=100),
-  "AdaBoost":      AdaBoostRegressor(n_estimators=100),
-  "Skl GBM":       GradientBoostingRegressor(n_estimators=100),
-  "XGBoost":       XGBRegressor(n_estimators=100),
-  "LightGBM":      LGBMRegressor(n_estimators=100),
-  "CatBoost":      CatBoostRegressor(n_estimators=100),
-}
-### END SOLUTIONv
 
-tree_classifiers = {name: pipeline.make_pipeline(tree_prepro, model) for name, model in tree_classifiers.items()}
+# rang = abs(y_train.max()) + abs(y_train.min())
 
-results = pd.DataFrame({'Model': [], 'MSE': [], 'MAB': [], " % error": [], 'Time': []})
+# num_4_treeModels = pipeline.Pipeline(steps=[
+#     ('imputer', impute.SimpleImputer(strategy='constant', fill_value=-9999)),
+# ])
 
-for model_name, model in tree_classifiers.items():
+# cat_4_treeModels = pipeline.Pipeline(steps=[
+#     ('imputer', impute.SimpleImputer(strategy='constant', fill_value='missing')),
+#     ('ordinal', preprocessing.OrdinalEncoder()) # handle_unknown='ignore' ONLY IN VERSION 0.24
+# ])
+
+# tree_prepro = compose.ColumnTransformer(transformers=[
+#     ('num', num_4_treeModels, num_vars),
+#     ('cat', cat_4_treeModels, cat_vars),
+# ], remainder='drop') # Drop other vars not specified in num_vars or cat_vars
+
+# tree_classifiers = {
+#   "Decision Tree": DecisionTreeRegressor(),
+#   "Extra Trees":   ExtraTreesRegressor(n_estimators=100),
+#   "Random Forest": RandomForestRegressor(n_estimators=100),
+#   "AdaBoost":      AdaBoostRegressor(n_estimators=100),
+#   "Skl GBM":       GradientBoostingRegressor(n_estimators=100),
+#   "XGBoost":       XGBRegressor(n_estimators=100),
+#   "LightGBM":      LGBMRegressor(n_estimators=100),
+#   "CatBoost":      CatBoostRegressor(n_estimators=100),
+# }
+# ### END SOLUTIONv
+
+# tree_classifiers = {name: pipeline.make_pipeline(tree_prepro, model) for name, model in tree_classifiers.items()}
+
+# results = pd.DataFrame({'Model': [], 'MSE': [], 'MAB': [], " % error": [], 'Time': []})
+
+# for model_name, model in tree_classifiers.items():
     
-    start_time = time.time()
-    model.fit(x_train, y_train)
-    total_time = time.time() - start_time
+#     start_time = time.time()
+#     model.fit(x_train, y_train)
+#     total_time = time.time() - start_time
         
-    pred = model.predict(x_val)
+#     pred = model.predict(x_val)
     
-    results = results.append({"Model":    model_name,
-                              "MSE": metrics.mean_squared_error(y_val, pred),
-                              "MAB": metrics.mean_absolute_error(y_val, pred),
-                              " % error": metrics.mean_squared_error(y_val, pred) / rang,
-                              "Time":     total_time},
-                              ignore_index=True)
-### END SOLUTION
+#     results = results.append({"Model":    model_name,
+#                               "MSE": metrics.mean_squared_error(y_val, pred),
+#                               "MAB": metrics.mean_absolute_error(y_val, pred),
+#                               " % error": metrics.mean_squared_error(y_val, pred) / rang,
+#                               "Time":     total_time},
+#                               ignore_index=True)
+# ### END SOLUTION
 
 
-results_ord = results.sort_values(by=['MSE'], ascending=True, ignore_index=True)
-results_ord.index += 1 
-results_ord.style.bar(subset=['MSE', 'MAE'], vmin=0, vmax=100, color='#5fba7d')
+# results_ord = results.sort_values(by=['MSE'], ascending=True, ignore_index=True)
+# results_ord.index += 1 
+# results_ord.style.bar(subset=['MSE', 'MAE'], vmin=0, vmax=100, color='#5fba7d')
 
 # print(results_ord)
 
