af0aed8ec3d91aefc6b0a94a99434cc8c5c0ad38
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-05-28 09:32:02 -0700

    Update train.py

--- Chapter 03/03. MLP Regression/train.py
+++ Chapter 03/03. MLP Regression/train.py
@@ -21,10 +21,10 @@ torch.manual_seed(0)
 
 pth='Chapter 03/03. MLP Regression/data/turkish_stocks.csv'
 
-X_train, X_test, y_train, y_test= dh.to_batches(pth, batch_size=4)
+X_train, X_test, y_train, y_test= dh.to_batches(pth, batch_size=8)
 
 #print(X_train)
-model = NeuralNetwork(8,8,4,2)
+model = NeuralNetwork(8,32,16,8)
 
 # Remember to validate your model: model.eval .........with torch.no_grad() ......model.train
 
@@ -51,7 +51,7 @@ def torch_fit(X_train,X_test,y_train,y_test, model, lr, num_epochs):
 
             running_loss += loss.item()
             if i % print_every == 0:
-                print(f"\tIteration: {i}\t Loss: {running_loss/print_every:.4f}")
+                #print(f"\tIteration: {i}\t Loss: {running_loss/print_every:.4f}")
                 running_loss = 0
 
         model.eval()
@@ -66,6 +66,7 @@ def torch_fit(X_train,X_test,y_train,y_test, model, lr, num_epochs):
         test_losses.append(mean_epoch_losses_test)
         mean_epoch_losses=sum(epoch_list)/len(epoch_list)
         train_lossses.append(mean_epoch_losses)
+
         print(f'Mean epoch loss for train{mean_epoch_losses}')
         print(f'Mean epoch loss for test{mean_epoch_losses_test}')
     
@@ -74,10 +75,8 @@ def torch_fit(X_train,X_test,y_train,y_test, model, lr, num_epochs):
     plt.plot(test_losses,label = 'Test Losses')
     plt.legend()
     plt.show()
-model = torch_fit(X_train=X_train,X_test=X_test, y_train=y_train,y_test=y_test,lr=0.002,num_epochs=30, model=model)
+model = torch_fit(X_train=X_train,X_test=X_test, y_train=y_train,y_test=y_test,lr=0.001,num_epochs=30, model=model)
 print('X_train',X_train.shape)
 print('y_train',y_train.shape)
 print('X_Test', X_test.shape)
 print('y_test',y_test.shape)
-
-
