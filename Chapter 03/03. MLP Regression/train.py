a87d630f8abc6e7e15f7c3f45c00d7e1b7c451ab
Abubakr Mamajonov <mamajonov1710@gmail.com>
2022-05-28 10:33:11 -0700

    Update train.py

--- Chapter 03/03. MLP Regression/train.py
+++ Chapter 03/03. MLP Regression/train.py
@@ -1,6 +1,7 @@
 #import the needed libraries
 
 
+from cProfile import label
 from pickletools import optimize
 from pyexpat import model
 from re import X
@@ -30,12 +31,14 @@ model = NeuralNetwork(8,32,16,8)
 
 def torch_fit(X_train,X_test,y_train,y_test, model, lr, num_epochs):
     
+   
     criterion = nn.L1Loss()
     optimizer = optim.SGD(model.parameters(),lr)
     print_every = 1
 
     train_lossses = []
     test_losses=[]
+    acc=[]
     for epoch in range(num_epochs):
         running_loss=0
 
@@ -57,22 +60,34 @@ def torch_fit(X_train,X_test,y_train,y_test, model, lr, num_epochs):
         model.eval()
         with torch.no_grad():
             test_epoch_list=[]
+            mape_list=[]
             for j, (X_test_batches,y_test_batches) in enumerate(zip(X_test,y_test)):
                 test_output=model.forward(X_test_batches)
                 test_loss=criterion(test_output,y_test_batches)
                 test_epoch_list.append(test_loss.item())
+                mape=torch.abs((y_test_batches[0]-X_test_batches[0]) / y_test_batches[0])*100
+                mape_list.append(mape)
+
+
+
+                #mape = np.abs((actual - predicted) / actual).mean(axis=0) * 100
 
         mean_epoch_losses_test=sum(test_epoch_list)/len(test_epoch_list)
         test_losses.append(mean_epoch_losses_test)
         mean_epoch_losses=sum(epoch_list)/len(epoch_list)
         train_lossses.append(mean_epoch_losses)
+        mean_mape = sum(mape_list)/len(mape_list)
+        acc.append(mean_mape)
+
 
         print(f'Mean epoch loss for train{mean_epoch_losses}')
         print(f'Mean epoch loss for test{mean_epoch_losses_test}')
+        print(f'Mean accuracy for epoch{mean_mape}')
     
 
     plt.plot(train_lossses,label = 'Train Losses')
     plt.plot(test_losses,label = 'Test Losses')
+    plt.plot(acc, label='Accuracy')
     plt.legend()
     plt.show()
 model = torch_fit(X_train=X_train,X_test=X_test, y_train=y_train,y_test=y_test,lr=0.001,num_epochs=30, model=model)
